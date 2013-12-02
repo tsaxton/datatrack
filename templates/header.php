6a35ea68de0ac03affadf6f10f35998941e24918
mmmccarthy <michaelmccarthy@Michaels-MacBook-Pro-4.local>
2013-12-01 19:02:22 -0600

    Switched to compressed scripts for loading time & home page mockup

--- templates/header.php
+++ templates/header.php
@@ -11,7 +11,7 @@ function dump($var, $label=NULL){
 <!DOCTYPE html>
 <html>
 <head>
-    <title>DataTrack:Chicago</title>
+    <title>DataTrack: Chicago</title>
     <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
 
     <!--Style-->
@@ -19,25 +19,26 @@ function dump($var, $label=NULL){
     <link rel="stylesheet" type="text/css" href="includes/css/jquery.jqplot.css"/>
     <link rel="stylesheet" type="text/css" href="includes/css/datatrack.css"/>
     <link href='http://fonts.googleapis.com/css?family=Roboto+Slab|Raleway:400,300' rel='stylesheet' type='text/css'>
-    <script src="http://code.jquery.com/jquery-1.9.1.js"></script>
     <link rel="stylesheet" href="http://code.jquery.com/ui/1.10.3/themes/smoothness/jquery-ui.css">
-
     <!--JavaScript-->
     <!--[if lt IE 9]><script language="javascript" type="text/javascript" src="excanvas.js"></script><![endif]-->
-    
-    <script src="http://code.jquery.com/ui/1.10.3/jquery-ui.js"></script>
-    <script language="javascript" type="text/javascript" src="includes/js/jquery.jqplot.min.js"></script>
+    <script src="http://code.jquery.com/jquery-1.10.2.min.js"></script>
+    <script src="http://code.jquery.com/ui/1.10.3/jquery-ui.min.js"></script>
     <script language="javascript" type="text/javascript" src="includes/js/bootstrap.min.js"></script>
+
+    
+    
+    <!--JavaScript for Data Analysis-->
+    <script language="javascript" type="text/javascript" src="includes/js/jquery.jqplot.min.js"></script>
     <script language="javascript" type="text/javascript" src="includes/js/plugins/jqplot.highlighter.min.js"></script>
     <script language="javascript" type="text/javascript" src="includes/js/plugins/jqplot.cursor.min.js"></script>
     <script language="javascript" type="text/javascript" src="includes/js/plugins/jqplot.dateAxisRenderer.min.js"></script>
-    <script language="javascript" type="text/javascript" src="includes/js/jquery.collapse.js"></script>
     <script language="javascript" type="text/javascript" src="includes/js/observations.js"></script>
 
 </head>
 <body>
 <div id="header">
-	<h1 id="title">DataTrack <span id="titleRed">CHICAGO</span></h1>
+	<h1 id="title">DataTrack <span id="titleRed">Chicago</span></h1>
 </div>
 <div id="menu">
 	<ul id="menuList">

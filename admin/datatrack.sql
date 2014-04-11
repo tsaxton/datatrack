bed9edcca798288efe306cb9a3698134f08d225c
Thomas M. Saxton <thomasmsaxton@gmail.com>
2014-04-11 15:31:35 -0500

    Finished adding new dataset to database, destroys web interface basically

--- admin/datatrack.sql
+++ admin/datatrack.sql
@@ -29,7 +29,7 @@ CREATE TABLE `categories` (
   PRIMARY KEY (`id`),
   KEY `dataset` (`dataset`),
   CONSTRAINT `categories_ibfk_1` FOREIGN KEY (`dataset`) REFERENCES `datasets` (`id`) ON DELETE CASCADE
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -38,7 +38,7 @@ CREATE TABLE `categories` (
 
 LOCK TABLES `categories` WRITE;
 /*!40000 ALTER TABLE `categories` DISABLE KEYS */;
-INSERT INTO `categories` VALUES (1,1,'Transportation'),(2,2,'Public Safety');
+INSERT INTO `categories` VALUES (1,1,'Transportation'),(2,2,'Public Safety'),(3,3,'Transportation'),(4,3,'Monthly'),(5,1,'Annual'),(6,2,'Annual');
 /*!40000 ALTER TABLE `categories` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -87,7 +87,7 @@ CREATE TABLE `fields` (
   PRIMARY KEY (`id`),
   KEY `dataset` (`dataset`),
   CONSTRAINT `fields_ibfk_1` FOREIGN KEY (`dataset`) REFERENCES `datasets` (`id`) ON DELETE CASCADE
-) ENGINE=InnoDB AUTO_INCREMENT=44 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=48 DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -96,34 +96,8 @@ CREATE TABLE `fields` (
 
 LOCK TABLES `fields` WRITE;
 /*!40000 ALTER TABLE `fields` DISABLE KEYS */;
-INSERT INTO `fields` VALUES (4,1,'total','Total Ridership',1),(5,1,'bus','Bus Ridership',1),(6,1,'rail','Elevated Train Ridership',1),(7,2,'OFFENSES INVOLVING CHILDREN','Offenses involving children',0),(8,2,'DOMESTIC VIOLENCE','Domestic violence',0),(9,2,'RITUALISM','Ritualism',0),(10,2,'INTERFERE WITH PUBLIC OFFICER','Interfere with public officer',0),(11,2,'NON-CRIMINAL (SUBJECT SPECIFIED)','Non-criminal (subject specified)',0),(12,2,'OTHER NARCOTIC VIOLATION','Other narcotic violation',0),(13,2,'NON-CRIMINAL','Non-criminal',0),(14,2,'OTHER OFFENSE ','Other offense ',0),(15,2,'OBSCENITY','Obscenity',0),(16,2,'PUBLIC INDECENCY','Public indecency',0),(17,2,'NON - CRIMINAL','Non - criminal',0),(18,2,'STALKING','Stalking',0),(19,2,'HOMICIDE','Homicide',1),(20,2,'KIDNAPPING','Kidnapping',0),(21,2,'INTIMIDATION','Intimidation',0),(22,2,'ARSON','Arson',1),(23,2,'SEX OFFENSE','Sex offense',0),(24,2,'PROSTITUTION','Prostitution',0),(25,2,'LIQUOR LAW VIOLATION','Liquor law violation',0),(26,2,'INTERFERENCE WITH PUBLIC OFFICER','Interference with public officer',0),(27,2,'CRIM SEXUAL ASSAULT','Criminal Sexual Assault',1),(28,2,'GAMBLING','Gambling',0),(29,2,'MOTOR VEHICLE THEFT','Motor vehicle theft',0),(30,2,'BURGLARY','Burglary',1),(31,2,'DECEPTIVE PRACTICE','Deceptive practice',0),(32,2,'OFFENSE INVOLVING CHILDREN','Offense involving children',0),(33,2,'ROBBERY','Robbery',1),(34,2,'WEAPONS VIOLATION','Weapons violation',0),(35,2,'NARCOTICS','Narcotics',0),(36,2,'PUBLIC PEACE VIOLATION','Public peace violation',0),(37,2,'CRIMINAL DAMAGE','Criminal damage',0),(38,2,'CRIMINAL TRESPASS','Criminal trespass',0),(39,2,'BATTERY','Battery',0),(40,2,'ASSAULT','Assault',1),(41,2,'OTHER OFFENSE','Other offense',0),(42,2,'THEFT','Theft',0),(43,2,'TOTAL','Total',0);
+INSERT INTO `fields` VALUES (4,1,'total','Total Ridership',1),(5,1,'bus','Bus Ridership',1),(6,1,'rail','Elevated Train Ridership',1),(7,2,'OFFENSES INVOLVING CHILDREN','Offenses involving children',0),(8,2,'DOMESTIC VIOLENCE','Domestic violence',0),(9,2,'RITUALISM','Ritualism',0),(10,2,'INTERFERE WITH PUBLIC OFFICER','Interfere with public officer',0),(11,2,'NON-CRIMINAL (SUBJECT SPECIFIED)','Non-criminal (subject specified)',0),(12,2,'OTHER NARCOTIC VIOLATION','Other narcotic violation',0),(13,2,'NON-CRIMINAL','Non-criminal',0),(14,2,'OTHER OFFENSE ','Other offense ',0),(15,2,'OBSCENITY','Obscenity',0),(16,2,'PUBLIC INDECENCY','Public indecency',0),(17,2,'NON - CRIMINAL','Non - criminal',0),(18,2,'STALKING','Stalking',0),(19,2,'HOMICIDE','Homicide',1),(20,2,'KIDNAPPING','Kidnapping',0),(21,2,'INTIMIDATION','Intimidation',0),(22,2,'ARSON','Arson',1),(23,2,'SEX OFFENSE','Sex offense',0),(24,2,'PROSTITUTION','Prostitution',0),(25,2,'LIQUOR LAW VIOLATION','Liquor law violation',0),(26,2,'INTERFERENCE WITH PUBLIC OFFICER','Interference with public officer',0),(27,2,'CRIM SEXUAL ASSAULT','Criminal Sexual Assault',1),(28,2,'GAMBLING','Gambling',0),(29,2,'MOTOR VEHICLE THEFT','Motor vehicle theft',0),(30,2,'BURGLARY','Burglary',1),(31,2,'DECEPTIVE PRACTICE','Deceptive practice',0),(32,2,'OFFENSE INVOLVING CHILDREN','Offense involving children',0),(33,2,'ROBBERY','Robbery',1),(34,2,'WEAPONS VIOLATION','Weapons violation',0),(35,2,'NARCOTICS','Narcotics',0),(36,2,'PUBLIC PEACE VIOLATION','Public peace violation',0),(37,2,'CRIMINAL DAMAGE','Criminal damage',0),(38,2,'CRIMINAL TRESPASS','Criminal trespass',0),(39,2,'BATTERY','Battery',0),(40,2,'ASSAULT','Assault',1),(41,2,'OTHER OFFENSE','Other offense',0),(42,2,'THEFT','Theft',0),(43,2,'TOTAL','Total',0),(44,3,'avg_weekday_rides','Average Weekday Rides',1),(45,3,'avg_saturday_rides','Average Saturday Rides',1),(46,3,'avg_sunday_holiday_rides','Average Sunday Rides',1),(47,3,'monthtotal','Total Rides',1);
 /*!40000 ALTER TABLE `fields` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
--- Table structure for table `fold`
---
-
-DROP TABLE IF EXISTS `fold`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `fold` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `dataset` int(11) NOT NULL,
-  `field` varchar(255) DEFAULT NULL,
-  PRIMARY KEY (`id`),
-  KEY `dataset` (`dataset`),
-  CONSTRAINT `fold_ibfk_1` FOREIGN KEY (`dataset`) REFERENCES `datasets` (`id`) ON DELETE CASCADE
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `fold`
---
-
-LOCK TABLES `fold` WRITE;
-/*!40000 ALTER TABLE `fold` DISABLE KEYS */;
-/*!40000 ALTER TABLE `fold` ENABLE KEYS */;
 UNLOCK TABLES;
 
 --
@@ -152,33 +126,6 @@ LOCK TABLES `foldSort` WRITE;
 /*!40000 ALTER TABLE `foldSort` DISABLE KEYS */;
 INSERT INTO `foldSort` VALUES (1,2,'primary_type','count_id');
 /*!40000 ALTER TABLE `foldSort` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
--- Table structure for table `foldYears`
---
-
-DROP TABLE IF EXISTS `foldYears`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `foldYears` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `dataset` int(11) NOT NULL,
-  `year` varchar(255) DEFAULT NULL,
-  `date` varchar(255) DEFAULT NULL,
-  PRIMARY KEY (`id`),
-  KEY `dataset` (`dataset`),
-  CONSTRAINT `foldYears_ibfk_1` FOREIGN KEY (`dataset`) REFERENCES `datasets` (`id`) ON DELETE CASCADE
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `foldYears`
---
-
-LOCK TABLES `foldYears` WRITE;
-/*!40000 ALTER TABLE `foldYears` DISABLE KEYS */;
-/*!40000 ALTER TABLE `foldYears` ENABLE KEYS */;
 UNLOCK TABLES;
 
 --
@@ -223,4 +170,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2014-04-11 15:11:12
+-- Dump completed on 2014-04-11 15:30:29

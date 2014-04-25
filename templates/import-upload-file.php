2cc87c6d0e7ad3813cf6d667c10df86f549dd006
Hanqing Ma <HanqingMa2013@u.northwestern.edu>
2014-04-25 00:24:15 -0500

    add data type radios

--- templates/import-upload-file.php
+++ templates/import-upload-file.php
@@ -5,6 +5,27 @@
     <input type="file" id="UploadFile">
     <p class="help-block">upload your file here.</p>
   </div>
+  
+<form id="data-type-selection" role="form">
+	<div class="radio-inline">
+		<label>
+			<input type="radio" id="data-type-monthly" checked name="data-type" value="monthly"/>
+				Monthly
+			</label>
+	</div>
+	<div class="radio-inline">
+		<label>
+			<input type="radio" id="data-type-Quarterly" name="data-type" value="Quarterly"/>
+				Quarterly
+			</label>
+	</div>
+	<div class="radio-inline">
+		<label>
+			<input type="radio" id="data-type--Yearly" name="data-type" value="Yearly"/>
+				Yearly
+		</label>
+	</div>
+</form>
 
   <button type='submit' class='btn btn-default'>upload</button>
 </form>

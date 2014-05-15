e35def4248de915174f6b5245303a65f86485c36
Thomas M. Saxton <thomasmsaxton@gmail.com>
2014-05-14 21:39:04 -0500

    Using remote database

--- admin/config.php
+++ admin/config.php
@@ -3,10 +3,15 @@ error_reporting(-1);
 ini_set('display_errors', 'on');
 
 
-$host = 'localhost';
+/*$host = 'localhost';
 $username = 'datatrack';
 $password = 'journalistdatadashboard';
-$dbName   = 'datatrack';
+$dbName   = 'datatrack';*/
+
+$host = 'us-cdbr-east-04.cleardb.com';
+$username = 'bb69d137447097';
+$password = '2015136f';
+$dbName = 'heroku_51bc4a95169dce1';
 
 $db = new MeekroDB($host, $username, $password, $dbName);
 
